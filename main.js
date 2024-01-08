27533471ae36c9a90780867fb8cb7671816ffb67
devshahed <shahedul872@gmail.com>
2024-01-08 20:33:26 +0600

    morphing

--- main.js
+++ main.js
@@ -1,5 +1,34 @@
 import LocomotiveScroll from 'locomotive-scroll';
 
+
+document.addEventListener('DOMContentLoaded', () => {
+    const interBubble = document.querySelector('.interactive');
+
+    let curX = 0;
+    let curY = 0;
+    let tgX = 0;
+    let tgY = 0;
+
+    function move() {
+        curX += (tgX - curX) / 20;
+        curY += (tgY - curY) / 20;
+        interBubble.style.transform = `translate(${Math.round(curX)}px, ${Math.round(curY)}px)`;
+        requestAnimationFrame(() => {
+            move();
+        });
+    }
+
+    window.addEventListener('mousemove', (event) => {
+        tgX = event.clientX;
+        tgY = event.clientY;
+    });
+
+    move();
+});
+
+
+
+
 const locomotiveScroll = new LocomotiveScroll();
 
 AOS.init({
