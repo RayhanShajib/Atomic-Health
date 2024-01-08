7062cc41e31513c7f908614a18d61c29438ed813
devshahed <shahedul872@gmail.com>
2024-01-08 20:28:26 +0600

    done

---
+++ main.js
@@ -0,0 +1,16 @@
+import LocomotiveScroll from 'locomotive-scroll';
+
+const locomotiveScroll = new LocomotiveScroll();
+
+AOS.init({
+    duration: 700,
+});
+const nameAnimationText = new SplitType('#animation')
+const nameAnimationText2 = new SplitType('#animation2')
+const nameAnimationText3 = new SplitType('#animation3')
+gsap.to('.char', {
+    y: 0,
+    stagger: 0.05,
+    delay: 0.2,
+    duration: .1
+})
